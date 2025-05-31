5c0c29a60ccc6318b15ec80e8ca98c70eb08b400
Await-0x <146035814+Await-0x@users.noreply.github.com>
2025-05-31 22:21:06 +0300

    null fix

--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -73,7 +73,9 @@ export default function GamePage() {
 
   useEffect(() => {
     return () => {
-      subscription?.cancel();
+      if (subscription) {
+        subscription.cancel();
+      }
       exitGame();
     };
   }, []);
